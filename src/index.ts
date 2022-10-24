3c3ddfba7d79e86c4d2a63fe082ce846f34f2d4a
JayJay1024 <lygm1062@gmail.com>
2022-10-24 17:30:43 +0800

    beta

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
-const main = () => {
-  console.log('Hey man')
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const endpoints = [
+  "https://api.subquery.network/sq/JayJay1024/crab-nft",
+  "https://api.subquery.network/sq/JayJay1024/darwinia-nft",
+];
+
+const query = gql`
+  query {
+    remarkedNftAddresses(first: 2) {
+      totalCount
+      nodes {
+        id
+        signer
+        value
+        addressValue
+      }
+    }
+  }
+`;
+
+const main = async () => {
+  for (const uri of endpoints) {
+    console.log(uri);
+    const client = new ApolloClient({ uri, cache: new InMemoryCache() });
+
+    try {
+      const res = await client.query({
+        query,
+      });
+      console.log(res);
+    } catch (_) {}
+  }
 };
 
 main();
